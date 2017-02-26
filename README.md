3b9a72d035d62c9f98466202e553f061f60add79
Juan <791171@gmail.com>
2017-02-25 18:58:10 -0800

    Filter Markers

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# Neighborhood-Map
+# Salinas Neighborhood Map
+
+## Overview
+This project uses JavaScript, jQuery, KnockoutJS, Google Maps API, and the Foursquare API to create a map of some locations of Salinas, CA.
+
+## Features
+- Responsive Layout
+- Foursquare Tips and Photos of Locations
+- Marker Animations
+
+## Basic Instruction
+- Open the index.html file in a modern browser (chrome, firefox, ie11, etc.).
+- Click markers or list items to select a location and retrieve info about it.
+- Type into the filter/search box to filter the shown locations.
+- Click page the small arrow at the bottom of the list to collapse or expand the list.
+- Click anywhere on the actual map to close the information window that opens.
+- Ensure options above filter/search box are what you want them to be.
--- js/app.js
+++ js/app.js
@@ -3,84 +3,83 @@ var Location = function(title, lng, lat) {
   this.title = title;
   this.lng = lng;
   this.lat = lat;
-    
   var venueId = foursquareVanues(lat,lng, title);
 
-// Return venueId of places matching the provided parameters
-function foursquareVanues(lat,lng, title) {
+  // Return venueId of places matching the provided parameters
+  function foursquareVanues(lat,lng, title) {
     var venueId;
     var data = $.ajax({
-        url: 'https://api.foursquare.com/v2/venues/search',
-        type:'GET',
-        dataType: 'JSON',
-        async:false,
-        data: {
+      url: 'https://api.foursquare.com/v2/venues/search',
+      type:'GET',
+      dataType: 'JSON',
+      async:false,
+      data: {
         ll:lng+","+lat,
         client_id: 'I5HFX2PIE5WFMHDNUV0C5R1EZAQVE0T03QFEMOY0YAMUCO4C', 
         client_secret: '0VDAZG1CNJPDGK25JESTAZGNPCCZK1NVPTHDCAVLJX4ZNYMI',
         v: '20150609'
-    }
+      }, error: function(){
+        alert("An error has occurred");
+      }   
     }).responseJSON;
-      $.each(data.response.venues, function(i, item){
-          if(item.name==title){
-              venueId = item.id;
-          }
-      });
+    $.each(data.response.venues, function(i, item){
+      if(item.name==title){
+        venueId = item.id;
+      }
+    });
     return venueId;
-    }
-    
- // Retrieve latest photos for this location marker
+  }
+
+  // Retrieve latest photos for this location marker
   this.foursquarePhotos = function() {
     var photos = [];
     var photossUrl = 'https://api.foursquare.com/v2/venues/' + venueId + '/photos';
     $.ajax({
-        url: photossUrl,
-        type:'GET',
-        dataType: 'JSON',
-        data: {
+      url: photossUrl,
+      type:'GET',
+      dataType: 'JSON',
+      data: {
         client_id: 'I5HFX2PIE5WFMHDNUV0C5R1EZAQVE0T03QFEMOY0YAMUCO4C', 
         client_secret: '0VDAZG1CNJPDGK25JESTAZGNPCCZK1NVPTHDCAVLJX4ZNYMI',
         v: '20150609',
         limit: 4,
         sort: 'recent'
-    }
+      }
     }).done(function(data){
       $.each(data.response.photos.items, function(i, photo){
         photos.push('<div class="photo"><img src='+photo.prefix+photo.width+photo.suffix+'></div>');
       });
       self.photos = '<div class="photos">' +  photos.join('') + '</div>';
     }).fail(function(jqXHR, textStatus) {
-      console.log('Ajax request failed! ' + textStatus);
+      alert(self.title+': Photos Error!');
     });
   }();
-    
+
   // Retrieve latest comments for this location marker
   this.foursquareComments = function() {
     var tips = [];
     var commentsUrl = 'https://api.foursquare.com/v2/venues/' + venueId + '/tips';
     $.ajax({
-        url: commentsUrl,
-        type:'GET',
-        dataType: 'JSON',
-        data: {
+      url: commentsUrl,
+      type:'GET',
+      dataType: 'JSON',
+      data: {
         client_id: 'I5HFX2PIE5WFMHDNUV0C5R1EZAQVE0T03QFEMOY0YAMUCO4C', 
         client_secret: '0VDAZG1CNJPDGK25JESTAZGNPCCZK1NVPTHDCAVLJX4ZNYMI',
         v: '20150609',
         limit: 5,
         sort: 'recent'
-    }
+      }
     }).done(function(data){
-
       $.each(data.response.tips.items, function(i, tip){
-          var user = '<strong>'+tip.user.firstName+'</strong>: ';
+        var user = '<strong>'+tip.user.firstName+'</strong>: ';
         tips.push('<li>' + user + tip.text + '</li>');
       });
       self.comments = '<h2>' + self.title + '</h2>' + '<h3>Recent Comments</h3>' + '<ol class="tips">' + tips.join('') + '</ol>';
     }).fail(function(jqXHR, textStatus) {
       self.comments = '<h2>' + self.title + '</h2>' + '<h3>Recent Comments</h3>' + '<h4>Oops. There was a problem retrieving this location\'s comments.</h4>';				
-      console.log('Ajax request failed! ' + textStatus);
+      alert(self.title+': Comments Error!');
     });
-      
   }();
 
   this.infowindow = new google.maps.InfoWindow();
@@ -88,17 +87,50 @@ function foursquareVanues(lat,lng, title) {
   this.marker = new google.maps.Marker({
     position: new google.maps.LatLng(self.lng, self.lat),
     map: map,
-    title: self.title,
+    title: self.title
   });
+
+  //self.marker.addListener('click', toggleBounce);
+
+  function toggleBounce() {
+    if (self.marker.getAnimation() !== null) {
+      self.marker.setAnimation(null);
+    } else {
+      self.marker.setAnimation(google.maps.Animation.BOUNCE);
+    }
+  }
+  this.isVisible = ko.observable(false);
+
+ // Display markers that are returned from the filtered searcg
+  this.isVisible.subscribe(function(currentState) {
+    if (currentState) {
+      self.marker.setMap(map);
+    } else {
+      self.marker.setMap(null);
+    }
+  });
+
+  this.isVisible(true);
 
   // Opens the info window for the location marker.
   this.openInfowindow = function() {
     for (var i=0; i < locations.locations.length; i++) {
       locations.locations[i].infowindow.close();
     }
-    map.panTo(self.marker.getPosition())
+    map.panTo(self.marker.getPosition());
     self.infowindow.setContent(self.photos + self.comments);
     self.infowindow.open(map,self.marker);
+      
+    // Add bouncing animation when a location is clicked
+    self.marker.setAnimation(google.maps.Animation.BOUNCE);
+    setTimeout(function() {
+      self.marker.setAnimation(null);
+    }, 2100);
+
+    this.bounce = function(place) {
+      google.maps.event.trigger(self.marker, 'click');
+    };
+
   };
 
   // Assigns a click event listener to the marker to open the info window.
@@ -124,7 +156,9 @@ locations.search = ko.dependentObservable(function() {
   var self = this;
   var search = this.query().toLowerCase();
   return ko.utils.arrayFilter(self.locations, function(location) {
-    return location.title.toLowerCase().indexOf(search) >= 0;
+    var result =  location.title.toLowerCase().indexOf(search) >= 0;
+    location.isVisible(result);
+    return result;
   });
 }, locations);
 
